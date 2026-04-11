1d5613a94afa18eb043f37a0e47b960bfbdbcd5b
Calpurniax <sofia.gracia@gmail.com>
2026-04-11 11:57:52 +0200

    added gsheet connection

---
+++ backend/backend/services/gsheet.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+def open_gsheet():
+    sheet = settings.GSCLIENT.open_by_key(settings.GSHEET).sheet1
+    return sheet
+
+def append_row_to_gsheet(row, sheet):
+    sheet.append_row(row)
