eeaa207408788c393b43d65d75ee314f49241917
Calpurniax <sofia.gracia@gmail.com>
2026-04-11 10:38:26 +0200

    created backend created usermodel

---
+++ backend/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class user(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    birthdate = models.DateField()
+    location ={
+        'latitude': models.FloatField(),
+        'longitude': models.FloatField(),
+        'city': models.CharField(max_length=100)
+    }
+    allergies = models.CharField(max_length=500)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.email
